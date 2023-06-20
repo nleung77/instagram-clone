dfec6a66e6b5c1db221d322623a3842dc091d3f5
nleung77 <nleung77@gmail.com>
2023-06-20 11:10:12 -0400

    added useEffect for login

--- instagram-clone/src/App.js
+++ instagram-clone/src/App.js
@@ -5,3 +5,27 @@ import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { auth } from "./firebase";
 import { loginUser, setLoading } from "./features/userSlice";
+
+function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      if(authUser) {
+        dispatch(
+          loginUser({
+            uid: authUser.uid,
+            username: authUser.displayName
+            email: authUser.email,
+          })
+        );
+        dispatch(setLoading(false));
+      } else {
+        dispatch(setLoading(false));
+        console.log("User is not logged in.");
+      }
+    });
+  }, []);
+  
+  
+}
