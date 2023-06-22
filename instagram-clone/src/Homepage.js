0670b92ff2c1eb860aec23fcd2c3828c07be0ca9
nleung77 <nleung77@gmail.com>
2023-06-21 22:19:53 -0400

    updated Homepage.js

---
+++ instagram-clone/src/Homepage.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Homepage.css";
+import Sidenav from "./navigation/Sidenav";
+import Timeline from "./timeline/Timeline";
+
+function Homepage() {
+  return (
+    <div className="homepage">
+      <div className="homepage_navWrapper">
+        <Sidenav />
+      </div>
+      <div className="homepage_timeline">
+        <Timeline />
+      </div>
+    </div>
+  );
+}
+
+export default Homepage;
