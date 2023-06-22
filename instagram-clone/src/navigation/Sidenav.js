b1abfde6d5e3ba6f253444da7f11914baebe73cd
nleung77 <nleung77@gmail.com>
2023-06-21 23:57:19 -0400

    sidenav,js dependencies

---
+++ instagram-clone/src/navigation/Sidenav.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Sidenav.css";
+import HomeIcon from "@mui/icons-material/Home";
+import SearchIcon from "@mui/icons-material/Search";
+import ExploreIcon from "@mui/icons-material/Explore";
+import SlideshowIcon from "@mui/icons-material/Slideshow";
+import ChatIcon from "@mui/icons-material/Chat";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
+import MenuIcon from "@mui/icons-material/Menu";
+import { Avatar } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+import { signOut } from "firebase/auth";
+import { logoutUser } from "../features/userSlice";
+import { auth } from "../firebase";
